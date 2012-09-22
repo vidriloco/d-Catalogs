c2d72db0405f4124a262a2b7f0e19de882f70f41
Alejandro Cruz Paz <vidriloco@gmail.com>
2012-09-22 09:53:46 -0500

    Added missing images to precompile queue

--- config/application.rb
+++ config/application.rb
@@ -58,6 +58,6 @@ module DCatalogs
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-    config.assets.precompile += ['application.js', 'application.css']
+    config.assets.precompile += ['application.js', 'application.css', 'apple-touch-icon-144x144-precomposed.png', 'apple-touch-icon-114x114-precomposed.png', 'images/apple-touch-icon-72x72-precomposed.png', 'images/apple-touch-icon-precomposed.png']
   end
 end
